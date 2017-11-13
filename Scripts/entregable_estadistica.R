c727d446ce3555605b8cf40aeb944ef8dce8d8a4
ferminlp93 <flp.novelda@gmail.com>
2017-11-13 21:35:01 +0100

    dataset limpio ej 1-2

--- Scripts/entregable_estadistica.R
+++ Scripts/entregable_estadistica.R
@@ -10,7 +10,8 @@
 
 #1. Importar el fichero JaenIndicadores.txt y denominar a la hoja de datos (data frame) Datos.Jaen
 Datos.Jaen <- read.table('./Datos/JaenIndicadores-utf8.txt',sep="\t", header=TRUE, encoding  ="UTF-8")
-
+#Limpiar datos faltantes
+Datos.Jaen <-Datos.Jaen[complete.cases(Datos.Jaen), ]
 
 #2. Recodificar la variable Poblacion en una variable cualitativa tipo factor llamada Tamaño con tres categorías:
 #  Si la población es inferior a 2000 habitantes, Tamaño será “Pequeño”.
@@ -28,4 +29,8 @@ Datos.Jaen$poblacioncat <- as.factor(Datos.Jaen$poblacioncat)
 Datos.Jaen$poblacion <- Datos.Jaen$poblacioncat 
 
 Datos.Jaen<-Datos.Jaen[1:8]
+
+#3. Calcular los siguientes promedios que se especifican a continuación y añadirlos como nuevas variables al fichero Datos.Jaen obtenidas a partir de las variables existentes: 
+#  Variable elec.hab que contendrá el consumo de energía eléctrica por habitante, obtenida como Consumo.de.energia.electrica/Poblacion
+#  Variable agua.hab que contendrá el consumo medio de agua por habitante y día, obtenida como (Consumo.de.agua..Invierno + Consumo.de.agua..V erano)/Poblacion
 
