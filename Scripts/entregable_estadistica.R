410a575d4a0790b33adbfcc9ea9f644de4976181
ferminlp93 <flp.novelda@gmail.com>
2017-11-14 18:33:14 +0100

    ejercicio 3 hecho

--- Scripts/entregable_estadistica.R
+++ Scripts/entregable_estadistica.R
@@ -1,6 +1,5 @@
 #####################1. Cálculo de nuevas variables, recodificación y filtrado#####################
 
-
 #Importamos el fichero de jaen
 #Código INE del municipio.
 #Nombre del municipio.
@@ -13,28 +12,39 @@
 #1. Importar el fichero JaenIndicadores.txt y denominar a la hoja de datos (data frame) Datos.Jaen
 Datos.Jaen <- read.table('./Datos/JaenIndicadores-utf8.txt',sep="\t", header=TRUE, encoding  ="UTF-8")
 #Limpiar datos faltantes
-Datos.Jaen <-Datos.Jaen[complete.cases(Datos.Jaen), ]
+Datos.Jaen <- Datos.Jaen[!Datos.Jaen$poblacion == "..", ]
+options(warn=-1)
+#Cambiar comas por puntos para poder calcular más tarde
+Datos.Jaen$residuoscant <- gsub("\\,", ".", Datos.Jaen$residuoscant)
+#Cambiar a numerico o doble las variables para los calculos
+Datos.Jaen$poblacion <- (as.numeric(as.character(Datos.Jaen$poblacion)))
+Datos.Jaen$consumoelec <- (as.numeric(as.character(Datos.Jaen$consumoelec)))
+Datos.Jaen$consumoaguainv <- (as.numeric(as.character(Datos.Jaen$consumoaguainv)))
+Datos.Jaen$consumoaguaver <- (as.numeric(as.character(Datos.Jaen$consumoaguaver)))
+Datos.Jaen$residuoscant <- (as.double(Datos.Jaen$residuoscant))
 
 #2. Recodificar la variable Poblacion en una variable cualitativa tipo factor llamada Tamaño con tres categorías:
 #  Si la población es inferior a 2000 habitantes, Tamaño será “Pequeño”.
 #  Si la población está entre 2000 y 4500 habitantes, Tamaño será “Mediano”.
 #  Si la población es superior a 4500 habitantes, Tamaño será “Grande”.
-options(warn=-1)
 
-Datos.Jaen$poblacioncat[(as.numeric(as.character(Datos.Jaen$poblacion)))<= 2000] <- "Pequeño"
 
-Datos.Jaen$poblacioncat[(as.numeric(as.character(Datos.Jaen$poblacion)))> 2000 & (as.numeric(as.character(Datos.Jaen$poblacion)))< 4500] <- "Mediano"
+Datos.Jaen$poblacioncat[Datos.Jaen$poblacion<= 2000] <- "Pequeño"
 
-Datos.Jaen$poblacioncat[(as.numeric(as.character(Datos.Jaen$poblacion)))>= 4500] <- "Grande"
+Datos.Jaen$poblacioncat[Datos.Jaen$poblacion> 2000 & (as.numeric(as.character(Datos.Jaen$poblacion)))< 4500] <- "Mediano"
+
+Datos.Jaen$poblacioncat[Datos.Jaen$poblacion>= 4500] <- "Grande"
 
 Datos.Jaen$poblacioncat <- as.factor(Datos.Jaen$poblacioncat)
 
-Datos.Jaen$poblacion <- Datos.Jaen$poblacioncat 
+#Datos.Jaen$poblacion <- Datos.Jaen$poblacioncat 
 
-Datos.Jaen<-Datos.Jaen[1:8]
+#Datos.Jaen<-Datos.Jaen[1:8]
 
 options(warn=0)
 #3. Calcular los siguientes promedios que se especifican a continuación y añadirlos como nuevas variables al fichero Datos.Jaen obtenidas a partir de las variables existentes: 
 #  Variable elec.hab que contendrá el consumo de energía eléctrica por habitante, obtenida como Consumo.de.energia.electrica/Poblacion
 #  Variable agua.hab que contendrá el consumo medio de agua por habitante y día, obtenida como (Consumo.de.agua..Invierno + Consumo.de.agua..V erano)/Poblacion
 
+Datos.Jaen$elec.hab <- Datos.Jaen$consumoelec/Datos.Jaen$poblacion
+Datos.Jaen$agua.hab <- (Datos.Jaen$consumoaguainv+Datos.Jaen$consumoaguaver)/Datos.Jaen$poblacion
